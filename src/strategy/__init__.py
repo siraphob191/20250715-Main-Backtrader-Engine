d585f4da5866b0284ffd893bd12e1f229f6a7b0c
siraphob191 <siraphob_siit@hotmail.com>
2025-07-15 22:07:39 +0700

    Restore nextstart and prenext

--- src/strategy/__init__.py
+++ src/strategy/__init__.py
@@ -1,4 +1,10 @@
 """Strategy package bundling core logic and utilities."""
 
-from .core import TestStrategy
+from .core import SVDMomentumStrategy, TestStrategy
 from .trend_filter import market_trend_filter
+
+__all__ = [
+    'SVDMomentumStrategy',
+    'TestStrategy',
+    'market_trend_filter',
+]
